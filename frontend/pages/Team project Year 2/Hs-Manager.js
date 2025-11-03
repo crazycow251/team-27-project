58f45494c9b302eecb718f546d2fb0dd8a0d9669
Thomas Davies <thomassamueldavies@gmail.com>
2025-11-03 12:27:19 +0000

    Most recent updates to final prototype

--- frontend/pages/Team project Year 2/Hs-Manager.js
+++
@@ -1,8 +0,0 @@
-// Hs-Manager.js
-import { ManagerDashboard } from './dashboard.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  const dashboard = new ManagerDashboard(root);
-  dashboard.init();
-});
